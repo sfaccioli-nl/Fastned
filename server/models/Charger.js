4d0175ae467875d7c31e83c5fff73969ab06a6f6
Sfaccioli <selefaccioli@gmail.com>
2022-09-19 10:28:36 +0200

    Tab changed to 2 spaces

--- server/models/Charger.js
+++ server/models/Charger.js
@@ -1,23 +1,23 @@
 import mongoose from 'mongoose';
 
 const chargerSchema = mongoose.Schema(
-    {
-        type: {
-            type: String,
-            enum: ['HPC', 'T52', 'T53C'],
-        },
-        serialNumber: {
-            type: String,
-            trim: true,
-        },
-        status: {
-            type: String,
-            enum: ['CONNECTED', 'NOT_CONNECTED', 'REMOVED'],
-        },
+  {
+    type: {
+      type: String,
+      enum: ['HPC', 'T52', 'T53C'],
     },
-    {
-        timestamps: true,
-    }
+    serialNumber: {
+      type: String,
+      trim: true,
+    },
+    status: {
+      type: String,
+      enum: ['CONNECTED', 'NOT_CONNECTED', 'REMOVED'],
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
 const Charger = mongoose.model('Charger', chargerSchema);
