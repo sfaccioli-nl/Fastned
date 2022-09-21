673dfe38c95ee719d024bfec8c0172302ac3b8f5
Sfaccioli <selefaccioli@gmail.com>
2022-09-21 16:18:55 +0200

    Generic popup

---
+++ client/src/components/Popup/Popup.tsx
@@ -0,0 +1,39 @@
+import { faClose } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Button from '../UI/Button/Button';
+import styles from './Popup.module.scss';
+
+interface IPopup {
+	visible: boolean;
+	setVisible: (arg: boolean) => void;
+	title: string;
+	content: any;
+	onSave?: () => void;
+}
+
+/**
+ * Popup component
+ */
+export default function Popup(props: IPopup): JSX.Element {
+	return (
+		<>
+			{props.visible && (
+				<div className={styles.container}>
+					<div className={styles.contentBox}>
+						<div className={styles.header}>
+							<p>{props.title}</p>
+							<FontAwesomeIcon icon={faClose} onClick={() => props.setVisible(false)} />
+						</div>
+						<div className={styles.content}>{props.content}</div>
+						<div className={styles.actions}>
+							<Button className="primary">Save</Button>
+							<Button className="secondary" onClick={() => props.setVisible(false)}>
+								Cancel
+							</Button>
+						</div>
+					</div>
+				</div>
+			)}
+		</>
+	);
+}
